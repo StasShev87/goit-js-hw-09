544ede50a64b3ba114332b798836acad763707bc
Stanislav Shevchenko <StasShev87@gmail.com>
2022-12-07 01:59:21 +0200

    Add color switcher refs

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,9 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
